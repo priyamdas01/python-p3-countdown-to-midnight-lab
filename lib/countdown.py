af7819555df078704c8e0543994f3940026c0f64
Priyam <12priyamdas34@gmail.com>
2023-07-05 20:38:10 -0400

    complete lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,20 @@
 # your code goes here!
+import time
+def countdown(argues):
+    args = argues
+    while args >= 0:
+        if args == 0:
+            print("HAPPY NEW YEAR!")
+        else:
+            print(f"{args} SECOND(S)!")
+        args -= 1
+
+def countdown_with_sleep(argues):
+    args = argues
+    while args >= 0:
+        if args == 0:
+            print("HAPPY NEW YEAR!")
+        else:
+            print(f"{args} SECOND(S)!")
+        time.sleep(1)
+        args -= 1
